25113b36e1c2512a87b60d7c9dfc999270e225ee
NiStudioUs <NiStudioUs@users.noreply.github.com>
2026-06-05 19:34:45 +0000

    deploy(portfolio): build @ 99c362788b61e19583974c7154cff3f2840902fa [skip ci] 99c362788b61e19583974c7154cff3f2840902fa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2221730179" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "293191720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
