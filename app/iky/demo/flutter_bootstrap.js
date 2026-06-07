65adb443ee7649d9085520fdbbc3c10c5bf22938
vskarthik9896 <vskarthik9896@users.noreply.github.com>
2026-06-07 08:50:52 +0000

    deploy(iky): Flutter web build @ 414e654eca24efe649a5b5125ff1d8dcbb5ef6ca [skip ci] vskarthik9896/people-app-iky-v2@414e654eca24efe649a5b5125ff1d8dcbb5ef6ca

--- app/iky/demo/flutter_bootstrap.js
+++ app/iky/demo/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"11168157fb1ac7c2be189361f845a3bb36b6e4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2076573455" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3253834956" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
